51f0ccf2a62e82d166ec7832fc0ab3c33c925322
George Charles <gkcharles@outlook.com>
2022-07-18 17:19:11 +0100

    testing remote module

---
+++ gklabs2/dir1/file1.go
@@ -0,0 +1,9 @@
+package package1
+
+import "fmt"
+
+var Name = "Kunle Ajayi"
+
+func Find(){
+	fmt.Printf("Find is running...%s", Name)
+}
---
+++ gklabs3/dir1/file1.go
@@ -0,0 +1,10 @@
+package package1
+
+import "fmt"
+
+var AnotherName = "Jasper Opopo"
+
+
+func AnotherFunc(){
+	fmt.Printf("AnotherFunc running...%s", AnotherName)
+}
